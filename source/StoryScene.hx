9466de591823635eb79bcf78e81d2fe69a857959
Random people <98900217+justaguy6@users.noreply.github.com>
2023-07-07 18:54:03 -0300

    Update StoryScene.hx

--- source/StoryScene.hx
+++ source/StoryScene.hx
@@ -3,8 +3,9 @@ import flixel.FlxSprite;
 
 class StoryScene extends MusicBeatState
 {
+	#if windows 
 	public var handler:MP4Handler;
-
+        #end
 	public var path:String = "";
 
 	public function new(bruh)
@@ -15,14 +16,18 @@ class StoryScene extends MusicBeatState
 
 	public override function load()
 	{
+		#if windows 
 		handler = new MP4Handler();
+		#end
 	}
 
 	public override function update(elapsed)
 	{
 		if (FlxG.keys.justPressed.ESCAPE)
 		{
+			#if windows 
 			handler.kill();
+			#end
 			switchState(new BruhADiagWindow(PlayState.SONG.songId));
 		}
 		super.update(elapsed);
@@ -30,11 +35,13 @@ class StoryScene extends MusicBeatState
 
 	public override function create()
 	{
+	        #if windows 
 		handler.playMP4(Paths.video(path));
 		handler.finishCallback = function()
 		{
 			switchState(new BruhADiagWindow(PlayState.SONG.songId));
 		};
+		#end
 		super.create();
 	}
 }
